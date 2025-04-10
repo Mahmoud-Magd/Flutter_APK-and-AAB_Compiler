6cfb6bdcc222e77134845b1e97f40461da758efa
Mahmoud-Magd <112110992+Mahmoud-Magd@users.noreply.github.com>
2025-04-11 01:39:53 +0200

    Update home.dart

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,17 +1,27 @@
 import 'package:flutter/material.dart';
-
-
+import 'widgets/input.dart'; // import your custom Input widget
 
 class HomePage extends StatelessWidget {
-    const HomePage ({super.key});
-    
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold (
-            appBar: AppBar(
-                title: Text('Breakfast'),
-                centerTitle: true,
-            ),
-        );
-    }
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Breakfast'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Input(
+          title: "Password",
+          hintText: "Enter password",
+          leftImage: "assets/icons/i_lock.png",
+          rightImage: "assets/icons/i_eye.png",
+          errorText: "Password is required",
+          forgotText: "Forgot password?",
+        ),
+      ),
+    );
+  }
 }
