24ff79cc5074dbd20e66a9e3f53476034241b5f0
Mahmoud-Magd <112110992+Mahmoud-Magd@users.noreply.github.com>
2025-04-11 03:41:58 +0200

    Update home.dart

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,7 +8,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Breakfast'),
+        title: const Text('Magd'),
         centerTitle: true,
       ),
       body: Padding(
